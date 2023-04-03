507f3128d572b6dd44fe1c7c66a08ab946e506ab
Sebastien <byasebastien@hotmail.com>
2023-04-03 12:40:31 +0200

    Add project files.

---
+++ Banque_NetCore2023/Models/Courant.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque_NetCore2023.Models
+{
+    public class Courant
+    {
+        private decimal _ligneDeCredit;
+
+        #region Props
+        public string Numero { get; set; }
+        public decimal Solde { get; private set; }
+        public decimal LigneDeCredit 
+        {
+            get
+            {
+                return _ligneDeCredit;
+            }
+            set
+            {
+                if (value < 0)
+                    return;
+                _ligneDeCredit = value;
+            }
+        }
+        #endregion
+        public Personne Titulaire { get; set; }
+
+        #region Methodes
+        /// <summary>
+        /// Retire un montant au Solde du compe
+        /// </summary>
+        /// <param name="montant">le montant à retirer au solde</param>
+        public void Retrait(decimal montant)
+        {
+            if (montant < 0)
+                return;
+            if (Solde - montant < -LigneDeCredit)
+                return;
+            Solde -= montant;
+        }
+
+        /// <summary>
+        /// Ajoute un montant au Solde du compe
+        /// </summary>
+        /// <param name="montant">le montant à ajouter au solde</param>
+        public void Depot(decimal montant)
+        {
+            if(montant < 0)
+                return;
+            Solde += montant;
+        }
+        #endregion
+    }
+}
---
+++ Banque_NetCore2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using Banque_NetCore2023.Models;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Courant c1 = new Courant()
+{
+    Numero = "1",
+    LigneDeCredit = 100,
+    Titulaire = new Personne()
+    {
+        Nom = "Bya",
+        Prenom = "Seb",
+        DateNaiss = new DateTime(1991,3,27)
+    }
+};
+Console.WriteLine($"Compte {c1.Numero} : {c1.Solde} €");
+c1.Depot(1000);
+Console.WriteLine("_______________________");
+Console.WriteLine($"Compte {c1.Numero} : {c1.Solde} €");
+c1.Retrait(100);
+Console.WriteLine("_______________________");
+Console.WriteLine($"Compte {c1.Numero} : {c1.Solde} €");
+c1.Titulaire.Prenom = "Toto";
